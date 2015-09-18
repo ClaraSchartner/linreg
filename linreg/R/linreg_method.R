8e4fc98d6731836fb27a17bcc1798db9bf57494e
Araya Eamrurksiri <araea382@student.liu.se>
2015-09-18 20:08:28 +0200

    commit

--- linreg/R/linreg_method.R
+++ linreg/R/linreg_method.R
@@ -1,9 +1,6 @@
-
-
 linreg <- function(x, ...){
     UseMethod("linreg")
 }
-
 
 print.linreg <- function(x, ...){
     cat("Call:\n")
@@ -12,27 +9,20 @@ print.linreg <- function(x, ...){
     print(x$coefficients)
 }
 
-
-
-
 linreg <- structure(list(), class = "linreg")
-##Test outside the function with faithful data
-X <- model.matrix(eruptions~waiting, data = faithful)
-y <- as.matrix(faithful[,1, drop = FALSE])
 
 #xlab is missing:fitted values 
 # http://stackoverflow.com/questions/13223846/ggplot2-two-line-label-with-expression
 #to make 2 lines
 plot.linreg <- function(x, ...){
-
-ggplot(data=x$fit.res,aes(x=fit,y=res))+geom_point()+
+    ggplot(data=x$fit.res,aes(x=fit,y=res))+geom_point()+
     geom_smooth(method = "loess", formula = y ~ x,se=FALSE, colour = "red") + 
     xlab(x$call) + ylab("residuals") + ggtitle("Residuals vs. Fitted")
- ggplot(data=x$fit.res,aes(x=fit,y=sqrt(abs(res))))+geom_point()+
-   geom_smooth(method = "loess", formula = y ~ x,se=FALSE, colour = "red") + 
- xlab(x$call) + ylab(expression(paste(sqrt("Standardized residuals")))) + ggtitle("Scale−Location")
+    ggplot(data=x$fit.res,aes(x=fit,y=sqrt(abs(res))))+geom_point()+
+    geom_smooth(method = "loess", formula = y ~ x,se=FALSE, colour = "red") + 
+    xlab(x$call) + ylab(expression(paste(sqrt("Standardized residuals")))) + ggtitle("Scale−Location")
 
- }
+}
 
 resid.linreg <- function(x, ...){
     return(res)
@@ -47,9 +37,8 @@ coef.linreg <- function(x, ...){
     return(x$coefficients)
 }
 
-
-   summary.linreg <- function(x, ...){
-     cat("Call:\n")
+summary.linreg <- function(x, ...){
+    cat("Call:\n")
     print(x$call) 
     cat("\nCoefficients:\n")
     sd_coeff <- sqrt(x$varcoeff)
@@ -59,8 +48,5 @@ coef.linreg <- function(x, ...){
     sd_res <- sqrt(x$varres)
     cat(sd_res, "on", x$df, "degree of freedom")
 }
-
-
-
 
 
