a461d24f24f1b91d977d18edf8b932d8fc7cb893
unknown <Clara.Schartner@gmail.com>
2015-09-18 20:09:34 +0200

    tests

--- linreg/tests/testthat/tests.R
+++ linreg/tests/testthat/tests.R
@@ -4,7 +4,7 @@ data("faithful")
 m<-lm(eruptions~waiting,faithful)
 n<- linreg(eruptions~waiting,faithful)
 test_that("incorrect",{
- expect_equal(sum(as.numeric(unlist(m[1]))-as.numeric(unlist(n))),0)
+ expect_less_than(sum(as.numeric(unlist(m[[1]]))-as.numeric(unlist(n[[1]]))),0.001)
 })
 
 
@@ -12,5 +12,5 @@ data("iris")
 lm.iris<-lm(Sepal.Length~Sepal.Width+Petal.Length^2,iris)
 lin.iris<-  linreg(Sepal.Length~Sepal.Width+Petal.Length^2,iris)
 test_that("cubic is correct",{
-expect_equal(sum(as.numeric(unlist(lm.iris[1]))-as.numeric(unlist(lin.iris))),0)
+expect_less_than(sum(as.numeric(unlist(lm.iris[[1]]))-as.numeric(unlist(lin.iris[[1]]))),0.001)
 })
